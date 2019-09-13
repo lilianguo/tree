721cc81581bf23c540a42bfaffc7e2a13440c868
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-12 22:29:25 -0700

    add tree manipulations

---
+++ FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class FlattenBinaryTreeToLinkedList {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            node.left = null;
+            if (stack.isEmpty()) {
+                node.right = null;
+            } else {
+                node.right = stack.peek();
+            }
+        }
+        return;
+    }
+}
